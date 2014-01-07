d84e43f348aca8c59ac3dae6310f51f535ca2c69
Stefan Natchev <stefan.natchev@gmail.com>
2014-01-07 14:43:50 -0800

    add test case causing of valid Ruby causing error in RubyParser

--- test/test_ruby_parser.rb
+++ test/test_ruby_parser.rb
@@ -292,6 +292,13 @@ module TestRubyParserShared
     assert_parse rb, pt
   end
 
+  def test_begin_else_return_value
+    rb = "begin; else 2; end"
+    pt = s(:block, s(:lit, 2))
+
+    assert_parse rb, pt
+  end
+
   def test_bug_comment_eq_begin
     rb = "\n\n#\n=begin\nblah\n=end\n\n"
     pt = nil
